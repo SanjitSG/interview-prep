534c7d5a52f770397d45529098dee15f534d44ad
sanjitsg <sanjeet25.sg@gmail.com>
2024-05-26 23:04:19 +0530

    hoisting

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-// var vs let vs const
+// // var vs let vs const
 
-// 1. scope: Scope refers to accessibility of variables, functions and objects. It determines where in your program you can use a particular piece of data.
+// // 1. scope: Scope refers to accessibility of variables, functions and objects. It determines where in your program you can use a particular piece of data.
 
-// Global Scope
-var a = 5;
+// // Global Scope
+// var a = 5;
 
-console.log(a);
+// console.log(a);
 
-{
-  //block scope
-  let b = 10;
-  console.log(b);
-}
+// {
+//   //block scope
+//   let b = 10;
+//   console.log(b);
+// }
 
-// shadowing - illegal shadowing
+// // 2. shadowing - illegal shadowing
 
-// Declaration - re-declaration
+// // 3. Declaration - re-declaration
 
-var a = 5;
-var a = 10;
+// var a = 5;
+// var a = 10;
 
-let a = 10;
-// let a = 12;   // not allowed
+// let a = 10;
+// // let a = 12;   // not allowed
 
-// initialization
+// // 4. initialization
 
-let a;
-// const b;  // not allowed
+// let a;
+// // const b;  // not allowed
 
-const b = 12;
+// const b = 12;
+
+// 5. Hoisting
+console.log(value);
+var value = 23;
