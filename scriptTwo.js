cb780dff582f0f6bd4a32d65842353abb9592f64
sanjitsg <sanjeet25.sg@gmail.com>
2024-05-29 22:50:48 +0530

    map vs forEach

--- scriptTwo.js
+++ scriptTwo.js
@@ -32,34 +32,48 @@
 
 // filter polyfill
 
-Array.prototype.myFilter = function (callBack) {
-  const temp = [];
+// Array.prototype.myFilter = function (callBack) {
+//   const temp = [];
 
-  for (let i = 0; i < this.length; i++) {
-    if (callBack(this[i], i, this)) temp.push(this[i]);
-  }
+//   for (let i = 0; i < this.length; i++) {
+//     if (callBack(this[i], i, this)) temp.push(this[i]);
+//   }
 
-  return temp;
-};
+//   return temp;
+// };
 
-const arr = [1, 2, 5, 6];
-const result = arr.myFilter((num) => {
-  return num > 5;
+// const arr = [1, 2, 5, 6];
+// const result = arr.myFilter((num) => {
+//   return num > 5;
+// });
+// console.log(result);
+
+// // reduce polyfill
+
+// Array.prototype.myReduce = function (callback, initialVal) {
+//   let accumulator = initialVal;
+//   for (let i = 0; i < this.length; i++) {
+//     accumulator = accumulator ? callback(accumulator, this[i], i, this) : this[i];
+//   }
+//   return accumulator;
+// };
+
+// const res = arr.myReduce((acc, curr) => {
+//   return acc + curr;
+// }, 0);
+
+// console.log(res);
+
+// Difference between map and forEach method
+
+arr = [1, 3, 5, 6];
+
+const mapRes = arr.map((ar) => {
+  return ar + 2;
 });
-console.log(result);
 
-// reduce polyfill
-
-Array.prototype.myReduce = function (callback, initialVal) {
-  let accumulator = initialVal;
-  for (let i = 0; i < this.length; i++) {
-    accumulator = accumulator ? callback(accumulator, this[i], i, this) : this[i];
-  }
-  return accumulator;
-};
-
-const res = arr.myReduce((acc, curr) => {
-  return acc + curr;
-}, 0);
-
-console.log(res);
+const forEachRes = arr.forEach((arr) => {
+  return arr + 3;
+});
+console.log(mapRes);
+console.log(forEachRes);
