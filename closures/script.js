0649991be6ba6c92e560b3c0ddcafec6b50dc52d
sanjitsg <sanjeet25.sg@gmail.com>
2024-06-02 23:51:17 +0530

    private counter

--- closures/script.js
+++ closures/script.js
@@ -57,3 +57,26 @@
 //   }
 // }
 // a();
+
+//Q5. How would you use a closure to create a private counter?
+
+function counter() {
+  let _counter = 0;
+
+  function add(num) {
+    _counter += num;
+  }
+
+  function retrieve() {
+    return "Counter " + _counter;
+  }
+
+  return {
+    add,
+    retrieve,
+  };
+}
+
+const c = counter();
+c.add(5);
+console.log(c.retrieve());
