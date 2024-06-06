cf7663c623f2fc027036c053f4b83dbe03e609e1
sanjitsg <sanjeet25.sg@gmail.com>
2024-06-06 23:41:58 +0530

    .

--- objects/script.js
+++ objects/script.js
@@ -1,27 +1,56 @@
 // Objects
 
-const user = {
-  name: "Sanjit",
-  age: 28,
-};
+// const user = {
+//   name: "Sanjit",
+//   age: 28,
+// };
 
-//access user property
-console.log(user.name);
-console.log(user["name"]);
+// //access user property
+// console.log(user.name);
+// console.log(user["name"]);
 
-// modify property
-user.name = "Sanjit Stark";
-console.log(user.name);
+// // modify property
+// user.name = "Sanjit Stark";
+// console.log(user.name);
 
-//delete property
-console.log("Before", user);
-delete user.age;
-console.log("After ", user);
+// //delete property
+// console.log("Before", user);
+// delete user.age;
+// console.log("After ", user);
 
 // Adding dynamic key value
-const property = "firstName";
-const val = "SanjitSG";
+// const property = "firstName";
+// const val = "SanjitSG";
 
-const newUser = {
-  [property]: val,
+// const newUser = {
+//   [property]: val,
+// };
+
+// console.log(newUser);
+
+/-------------FAQ-----------/;
+// 1. what will be the output
+const func = (function (a) {
+  delete a;
+  return a;
+})(5);
+console.log(func); // 5
+
+// 2. create a function multiplyByTwo(obj) that multiplies all the numeric property values of obj by 2.
+
+const obj = {
+  a: 200,
+  b: 300,
+  title: "My Obj",
 };
+
+function multiplyByTwo(obj) {
+  for (key in obj) {
+    if (typeof obj[key] === "number") {
+      obj[key] = obj[key] * 2;
+    }
+  }
+}
+
+multiplyByTwo(obj);
+console.log(obj);
