483b361673c19dc6c7e20258c3cdbbc88184a715
sanjitsg <sanjeet25.sg@gmail.com>
2024-06-06 23:28:44 +0530

    object basics

--- objects/script.js
+++ objects/script.js
@@ -1,1 +1,27 @@
 // Objects
+
+const user = {
+  name: "Sanjit",
+  age: 28,
+};
+
+//access user property
+console.log(user.name);
+console.log(user["name"]);
+
+// modify property
+user.name = "Sanjit Stark";
+console.log(user.name);
+
+//delete property
+console.log("Before", user);
+delete user.age;
+console.log("After ", user);
+
+// Adding dynamic key value
+const property = "firstName";
+const val = "SanjitSG";
+
+const newUser = {
+  [property]: val,
+};
