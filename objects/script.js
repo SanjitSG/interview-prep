a392d1679f0b00d9b52d192efb5b9b5dbc3b3688
sanjitsg <sanjeet25.sg@gmail.com>
2024-06-07 22:21:30 +0530

    object faq

--- objects/script.js
+++ objects/script.js
@@ -30,27 +30,39 @@
 
 /-------------FAQ-----------/;
 // 1. what will be the output
-const func = (function (a) {
-  delete a;
-  return a;
-})(5);
-console.log(func); // 5
+// const func = (function (a) {
+//   delete a;
+//   return a;
+// })(5);
+// console.log(func); // 5
 
 // 2. create a function multiplyByTwo(obj) that multiplies all the numeric property values of obj by 2.
 
-const obj = {
-  a: 200,
-  b: 300,
-  title: "My Obj",
-};
+// const obj = {
+//   a: 200,
+//   b: 300,
+//   title: "My Obj",
+// };
 
-function multiplyByTwo(obj) {
-  for (key in obj) {
-    if (typeof obj[key] === "number") {
-      obj[key] = obj[key] * 2;
-    }
-  }
-}
+// function multiplyByTwo(obj) {
+//   for (key in obj) {
+//     if (typeof obj[key] === "number") {
+//       obj[key] = obj[key] * 2;
+//     }
+//   }
+// }
 
-multiplyByTwo(obj);
-console.log(obj);
+// multiplyByTwo(obj);
+// console.log(obj);
+
+// 3. What will be the output?
+
+// const a = {};
+// const b = { key: "b" };
+// const c = { key: "c" };
+
+// a[b] = 123;
+// a[c] = 456;
+// console.log(a); // {[object object]: 456}
+
+// 4. what is JSON.stringfy and JSON.parse
