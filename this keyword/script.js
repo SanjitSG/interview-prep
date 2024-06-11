dd0a02fab623d67ac9169f5e02c68cc1989e43be
sanjitsg <sanjeet25.sg@gmail.com>
2024-06-11 23:38:10 +0530

    'this' keyword

---
+++ this keyword/script.js
@@ -0,0 +1,38 @@
+// 'this' keyword
+
+// let calc = {
+//   total: 0,
+//   add(a) {
+//     this.total += a;
+//     return this;
+//   },
+// };
+
+// console.log(calc.add(20));
+
+// --> 'this; inside class constructor points to constructor function variables.
+
+// class User {
+//   constructor(a) {
+//     this.name = a;
+//   }
+
+//   getName() {
+//     console.log(this.name);
+//   }
+// }
+
+// const newUser = new User("Steve");
+// newUser.getName();
+
+// Q - what will be th output
+
+const user = {
+  firstName: "Sanjit",
+  getName() {
+    const firstName = "Tim";
+    return this.firstName;
+  },
+};
+
+console.log(user.getName()); // "Sanjit" since this points to object inside a method.
